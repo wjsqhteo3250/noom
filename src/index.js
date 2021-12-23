aece2fbbe3bdfe5d96ee1fd463077faf3565570e
wjsqhteo3250 <jeon@LAPTOP-UOF3SOGR.localdomain>
2021-12-24 01:19:20 +0900

    done with setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import express from "express";
 const app = express();
 
 app.set("view engine", "pug");
-
 app.set("views", process.cwd() + "/src/views");
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (_, res) => {
     res.render("home");
+})
+app.get("/*", (_, res) => {
+    res.redirect("/");
 })
 
 app.listen(4000, () => { console.log("listening on port 4000") });
