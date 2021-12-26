2fb97804d229815cc053ed970f7f1ebd8108e735
wjsqhteo3250 <jeon@LAPTOP-UOF3SOGR.localdomain>
2021-12-27 02:27:38 +0900

    done with basic webSocket implement

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import http from "http";
 import express from "express";
+import morgan from "morgan";
+import WebSocket from "ws";
+
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use("/public", express.static(__dirname + "/public"));
+app.use(morgan("dev"));
 
 app.get("/", (_, res) => {
     res.render("home");
@@ -13,4 +18,30 @@ app.get("/*", (_, res) => {
     res.redirect("/");
 })
 
-app.listen(4000, () => { console.log("listening on port 4000") });
+const server = http.createServer(app);
+const ws = new WebSocket.Server({ server });
+
+const socketDB = [];
+
+ws.on("connection", (socket) => {
+    socketDB.push(socket);
+    socket["nickName"] = "Anon";
+    console.log("socket connect");
+    socket.on("close", () => {
+        console.log("disconnect from browser");
+    })
+    socket.on("message", (m) => {
+        const parsedMsg = JSON.parse(m);
+        switch (parsedMsg.type) {
+            case "newMsg":
+                socketDB.forEach(aSocket => aSocket.send(`${socket["nickName"]} : ${parsedMsg.payload}`));
+                break;
+
+            case "nickName":
+                socket["nickName"] = parsedMsg.payload;
+                break;
+        }
+    })
+})
+
+server.listen(4000, () => { console.log("listening on port 4000") });
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,43 @@
-console.log("front")
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+const nickForm = document.getElementById("nickForm");
+const chatList = document.getElementById("chatList");
+const msgForm = document.getElementById("msgForm");
+
+function stringifyToJson(type, payload) {
+    const obj = { type, payload }
+    return JSON.stringify(obj);
+}
+
+socket.addEventListener("open", () => {
+    console.log("socket connect form server");
+})
+
+socket.addEventListener("close", () => {
+    console.log("socket disconnect from server");
+})
+
+socket.addEventListener("message", (m) => {
+    const li = document.createElement("li");
+    li.innerText = m.data;
+    chatList.append(li);
+})
+
+const handleSendMsg = (e) => {
+    e.preventDefault();
+    const input = msgForm.querySelector("input[type=text]");
+    const msgObj = stringifyToJson("newMsg", input.value);
+    socket.send(msgObj);
+    input.value = "";
+}
+
+const handleSendNick = (e) => {
+    e.preventDefault();
+    const input = nickForm.querySelector("input[type=text]");
+    const msgObj = stringifyToJson("nickName", input.value);
+    socket.send(msgObj);
+    input.value = "";
+}
+
+msgForm.addEventListener("submit", handleSendMsg);
+nickForm.addEventListener("submit", handleSendNick);
